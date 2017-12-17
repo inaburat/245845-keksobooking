7bbfda5319dd1745f13fc80730347119a9627ce5
Василий Игнатов <inaburat@gmail.com>
2017-12-17 22:50:19 +0300

    drag and drop

--- js/map.js
+++ js/map.js
@@ -2,14 +2,15 @@
 
 (function () {
   var buttonFragment = document.createDocumentFragment(); // область для клонирования элементов
-
-
   var map = document.querySelector('.map');
   var mapPins = document.querySelector('.map__pins'); // область для открисовки новых кнопок
   var mapMainPin = document.querySelector('.map__pin--main');
   var mapForm = document.querySelector('.notice__form');
   var allFormElement = document.querySelectorAll('fieldset');
   var dataArray = [];
+
+  var MIN_CLIENT_Y = 100;
+  var MAX_CLIENT_Y = 500;
 
   var activateMap = function () {
     mapForm.classList.remove('notice__form--disabled');
@@ -19,6 +20,72 @@
     for (var i = 0; i < allFormElement.length; i++) {
       allFormElement[i].disabled = false;
     }
+    document.removeEventListener('mouseup', activateMap);
+  };
+
+  var dragAndDrop = function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.pageY
+    };
+    var endCoordsX = evt.clientX;
+    var endCoordsY = evt.pageY;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      var mouseClientX = moveEvt.clientX;
+      var mouseClientY = moveEvt.pageY;
+      var mapMinLeft = map.offsetLeft;
+      var mapWidth = map.offsetWidth;
+      var mainPinWidth = mapMainPin.offsetWidth;
+      var centerMainPin = mainPinWidth / 2;
+      var minClientX = mapMinLeft + centerMainPin;
+      var maxClientX = (mapWidth + mapMinLeft) - centerMainPin;
+
+      var shift = {
+        x: startCoords.x - mouseClientX,
+        y: startCoords.y - mouseClientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.pageY
+      };
+
+      if (mouseClientX <= minClientX) {
+        endCoordsX = centerMainPin;
+      } else if (mouseClientX >= maxClientX) {
+        endCoordsX = mapWidth - centerMainPin;
+      } else {
+        endCoordsX = mapMainPin.offsetLeft - shift.x;
+      }
+
+      if (mouseClientY <= MIN_CLIENT_Y) {
+        endCoordsY = MIN_CLIENT_Y;
+      } else if (mouseClientY >= MAX_CLIENT_Y) {
+        endCoordsY = MAX_CLIENT_Y;
+      } else {
+        endCoordsY = mapMainPin.offsetTop - shift.y;
+      }
+
+      mapMainPin.style.left = (endCoordsX) + 'px';
+      mapMainPin.style.top = (endCoordsY) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      var inputAddress = document.querySelector('#address');
+
+      inputAddress.value = 'x: ' + endCoordsX + ', y: ' + endCoordsY;
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+    document.addEventListener('mouseup', activateMap);
   };
 
   var getDataById = function (index) {
@@ -34,7 +101,7 @@
       allFormElement[i].disabled = true;
     }
     mapForm.classList.add('notice__form--disabled');
-    mapMainPin.addEventListener('mouseup', activateMap);
+    mapMainPin.addEventListener('mousedown', dragAndDrop);
   };
   init();
 
